35ad7fe91b8648d42c2700fb7c30930f821bbbab
claygro <dhamala80130@gmail.com>
2026-08-01 16:12:34 +0545

    created upload schema and models

--- backend/src/controllers/upload.controllers.js
+++ backend/src/controllers/upload.controllers.js
@@ -1,2 +1,12 @@
-class UploadControllers {}
+class UploadControllers {
+  async upload(req, res) {
+    const { title, code, description } = req.body;
+    try {
+    } catch (error) {
+      resizeBy
+        .status(500)
+        .json({ message: `Failed to upload the code: ${error}}` });
+    }
+  }
+}
 export default UploadControllers;
---
+++ backend/src/models/upload.models.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const uploadSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    title: String,
+    code: String,
+    description: String,
+  },
+  { timestamps: true },
+);
+const UploadModel = mongoose.model("upload", uploadSchema);
+export default UploadModel;
